ba5c344ae93adb5364e7f4b8a283c34776ae79ae
TETvega <hectormartinez1vg@gmail.com>
2024-03-20 14:18:48 -0600

    add index, primary, and pages alternatives

---
+++ DOM-18/js/btn.js
@@ -0,0 +1,8 @@
+
+// Boton de iniciar secion
+var btnIniciarSesion = document.getElementById("BTN_IniciarSesion");
+
+btnIniciarSesion.addEventListener("click", function() {
+    // Redirigir a la página de inicio de sesión
+    window.location.href = "login.html";
+});
